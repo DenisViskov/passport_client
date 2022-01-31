d248a5916a57149cc14aa71d89b930f6b3a8513b
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-01-31 09:07:32 +0300

    Initial commit

---
+++ src/test/java/com/example/passport_client/PassportClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passport_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassportClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
