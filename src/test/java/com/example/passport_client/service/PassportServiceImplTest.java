05d0a31a81d957f9b068d88d16881312382968da
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-02-03 09:56:34 +0300

    some

--- src/test/java/com/example/passport_client/service/PassportServiceImplTest.java
+++ src/test/java/com/example/passport_client/service/PassportServiceImplTest.java
@@ -1,8 +1,11 @@
 package com.example.passport_client.service;
 
+import com.example.passport_client.config.PassportServiceApiInfoHolder;
 import com.example.passport_client.dto.PassportDto;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.assertj.core.api.Condition;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,6 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
@@ -18,6 +23,9 @@ class PassportServiceImplTest implements WithAssertions {
 
     @Autowired
     private PassportService<PassportDto> service;
+
+    @Autowired
+    private PassportServiceApiInfoHolder infoHolder;
 
     private static MockWebServer WEB_SERVER;
 
@@ -28,7 +36,7 @@ class PassportServiceImplTest implements WithAssertions {
     }
 
     @Test
-    void save() {
+    void save() throws InterruptedException {
         final Long expected = 1L;
         WEB_SERVER.enqueue(new MockResponse()
             .setBody(String.valueOf(expected))
@@ -36,9 +44,20 @@ class PassportServiceImplTest implements WithAssertions {
         );
 
         final Long result = service.save(PassportDto.builder().build());
+        final RecordedRequest recordedRequest = WEB_SERVER.takeRequest();
 
         assertThat(result).isNotNull()
             .isEqualTo(expected);
+
+        assertThat(recordedRequest.getMethod()).isNotNull()
+            .isEqualToIgnoringCase("post");
+
+        assertThat(recordedRequest.getPath()).isNotNull()
+            .isEqualTo(
+                "%s%s",
+                infoHolder.getEntry().getPassport(),
+                infoHolder.getEntry().getSave()
+            );
     }
 
     @Test
