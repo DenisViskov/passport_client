6b1e98cd859e6961c06f0347a49e26d9a454597d
Denis.Viskov <Denis.Viskov@nordigy.ru>
2022-02-08 19:50:35 +0300

    some

--- src/test/java/com/example/passport_client/api/PassportClientApiTest.java
+++ src/test/java/com/example/passport_client/api/PassportClientApiTest.java
@@ -4,25 +4,20 @@ import com.example.passport_client.StubBuilder;
 import com.example.passport_client.config.PassportServiceApiInfoHolder;
 import com.example.passport_client.dto.PassportDto;
 import com.example.passport_client.service.PassportService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
@@ -54,12 +49,11 @@ class PassportClientApiTest {
         final String expectedContent = result.toString();
         when(service.save(any())).thenReturn(result);
 
-        mockMvc.perform(post(ROOT_PATH.concat(apiHolder.getEntry().getSave()))
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(toSave))
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().string(expectedContent));
+        assertResponse(
+            post(ROOT_PATH.concat(apiHolder.getEntry().getSave()))
+                .content(mapper.writeValueAsString(toSave)),
+            expectedContent
+        );
     }
 
     @Test
@@ -69,13 +63,12 @@ class PassportClientApiTest {
         final String expectedContent = String.valueOf(result);
         when(service.update(any(), anyLong())).thenReturn(result);
 
-        mockMvc.perform(put(ROOT_PATH.concat(apiHolder.getEntry().getUpdate()))
+        assertResponse(
+            put(ROOT_PATH.concat(apiHolder.getEntry().getUpdate()))
                 .queryParam("id", "1")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(toUpdate))
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().string(expectedContent));
+                .content(mapper.writeValueAsString(toUpdate)),
+            expectedContent
+        );
     }
 
     @Test
@@ -84,12 +77,11 @@ class PassportClientApiTest {
         final String expectedContent = String.valueOf(result);
         when(service.delete(anyLong())).thenReturn(result);
 
-        mockMvc.perform(delete(ROOT_PATH.concat(apiHolder.getEntry().getDelete()))
-                .queryParam("id", "1")
-                .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().string(expectedContent));
+        assertResponse(
+            delete(ROOT_PATH.concat(apiHolder.getEntry().getDelete()))
+                .queryParam("id", "1"),
+            expectedContent
+        );
     }
 
     @Test
@@ -98,26 +90,28 @@ class PassportClientApiTest {
         final String expectedContent = mapper.writeValueAsString(expectedList);
         when(service.findAll()).thenReturn(expectedList);
 
-        mockMvc.perform(get(ROOT_PATH.concat(apiHolder.getEntry().getFind()))
-                .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().string(expectedContent));
+        assertResponse(
+            get(ROOT_PATH.concat(apiHolder.getEntry().getFind())),
+            expectedContent
+        );
     }
 
     @Test
     void findBySerial() throws Exception {
         final Long serial = 1234L;
-        final List<PassportDto> expectedList = Collections.singletonList(StubBuilder.builder().serial(serial).build());
+        final List<PassportDto> expectedList = Collections.singletonList(
+            StubBuilder.builder()
+                .serial(serial)
+                .build()
+        );
         final String expectedContent = mapper.writeValueAsString(expectedList);
         when(service.findBySerial(anyLong())).thenReturn(expectedList);
 
-        mockMvc.perform(get(ROOT_PATH.concat(apiHolder.getEntry().getFindBySerial()))
-                .queryParam("serial", serial.toString())
-                .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().string(expectedContent));
+        assertResponse(
+            get(ROOT_PATH.concat(apiHolder.getEntry().getFindBySerial()))
+                .queryParam("serial", serial.toString()),
+            expectedContent
+        );
     }
 
     @Test
@@ -130,14 +124,35 @@ class PassportClientApiTest {
         final String expectedContent = mapper.writeValueAsString(expectedList);
         when(service.findUnavailable()).thenReturn(expectedList);
 
-        mockMvc.perform(get(ROOT_PATH.concat(apiHolder.getEntry().getUnavailable()))
-                .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isOk())
-            .andExpect(content().string(expectedContent));
+        assertResponse(
+            get(ROOT_PATH.concat(apiHolder.getEntry().getUnavailable())),
+            expectedContent
+        );
     }
 
     @Test
-    void findReplaceable() {
+    void findReplaceable() throws Exception {
+        final List<PassportDto> expectedList = Collections.singletonList(
+            StubBuilder.builder()
+                .expiredDate(LocalDate.now().plusMonths(1))
+                .build()
+        );
+        final String expectedContent = mapper.writeValueAsString(expectedList);
+        when(service.findReplaceable()).thenReturn(expectedList);
+
+        assertResponse(
+            get(ROOT_PATH.concat(apiHolder.getEntry().getFindReplaceable())),
+            expectedContent
+        );
+    }
+
+    private void assertResponse(
+        final MockHttpServletRequestBuilder reqBuilder,
+        final String expectedContent
+    ) throws Exception {
+        mockMvc.perform(reqBuilder.contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().string(expectedContent));
     }
 }
